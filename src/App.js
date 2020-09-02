a91345f689de2fa855a52759082f3097794cdf94
Pedro Machado <machadop1407@gmail.com>
2020-09-02 14:56:46 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import PayPal from "./components/PayPal";
 
 function App() {
+  const [checkout, setCheckOut] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {checkout ? (
+        <PayPal />
+      ) : (
+        <button
+          onClick={() => {
+            setCheckOut(true);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Checkout
+        </button>
+      )}
     </div>
   );
 }
---
+++ src/components/PayPal.js
@@ -0,0 +1,39 @@
+import React, { useRef, useEffect } from "react";
+
+export default function Paypal() {
+  const paypal = useRef();
+
+  useEffect(() => {
+    window.paypal
+      .Buttons({
+        createOrder: (data, actions, err) => {
+          return actions.order.create({
+            intent: "CAPTURE",
+            purchase_units: [
+              {
+                description: "Cool looking table",
+                amount: {
+                  currency_code: "CAD",
+                  value: 650.0,
+                },
+              },
+            ],
+          });
+        },
+        onApprove: async (data, actions) => {
+          const order = await actions.order.capture();
+          console.log(order);
+        },
+        onError: (err) => {
+          console.log(err);
+        },
+      })
+      .render(paypal.current);
+  }, []);
+
+  return (
+    <div>
+      <div ref={paypal}></div>
+    </div>
+  );
+}
